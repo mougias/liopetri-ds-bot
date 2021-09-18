ff47ab55a83dea75e7fef42ca50aee666fbb5a91
Stefanos Demetriou <gcmougias@gmail.com>
2021-09-18 10:20:27 +0300

    NW countdown language per channel

--- src/main/java/com/stefanosdemetriou/discord/messaging/NewWorldCountdown.java
+++ src/main/java/com/stefanosdemetriou/discord/messaging/NewWorldCountdown.java
@@ -1,9 +1,6 @@
 package com.stefanosdemetriou.discord.messaging;
 
-import java.time.LocalDate;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,14 +9,12 @@ import java.util.concurrent.ScheduledFuture;
 import javax.annotation.PostConstruct;
 
 import org.jline.utils.Log;
-import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
 
 import discord4j.core.GatewayDiscordClient;
-import discord4j.rest.entity.RestChannel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,10 +27,8 @@ public class NewWorldCountdown implements DisposableBean {
 	private final TaskScheduler scheduler;
 
 	private static final List<ServerChannelPair> channels = List.of(
-			new ServerChannelPair(258369598833950723L, 582582345790521364L),
-			new ServerChannelPair(880572111196925974L, 880572111968686102L));
-	private static final LocalDate NW_RELEASE_DATE = LocalDate.parse("20210928",
-			DateTimeFormatter.ofPattern("yyyyMMdd"));
+			new ServerChannelPair(258369598833950723L, 582582345790521364L, "el"),
+			new ServerChannelPair(880572111196925974L, 880572111968686102L, "en"));
 
 	private List<ScheduledFuture<Void>> cronJobs = new ArrayList<>();
 
@@ -43,20 +36,8 @@ public class NewWorldCountdown implements DisposableBean {
 	@PostConstruct
 	public void init() {
 		for (var pair : channels) {
-
-			final var server = this.client.getGuildById(pair.getServerId()).block();
-			if (server == null) {
-				throw new BeanInitializationException("Could not get discord server by its id");
-			}
-
-			final var channel = server.getChannelById(pair.getChannelId()).block();
-			if (channel == null) {
-				throw new BeanInitializationException("Could not get discord channel by its id");
-			}
-
-			this.cronJobs.add(
-					(ScheduledFuture<Void>) this.scheduler.schedule(() -> this.sendCountdown(channel.getRestChannel()),
-							new CronTrigger("0 0 6 * * *", ZoneId.of("UTC"))));
+			this.cronJobs.add((ScheduledFuture<Void>) this.scheduler.schedule(() -> this.sendCountdown(pair),
+					new CronTrigger("0 0 6 * * *", ZoneId.of("UTC"))));
 		}
 
 		this.scheduler.schedule(this::cancelCountDown, new Date(1632834000000L));
@@ -64,21 +45,25 @@ public class NewWorldCountdown implements DisposableBean {
 		Log.info("New World countdown scheduled");
 	}
 
-	public void sendCountdown(RestChannel channel) {
-		if (channel == null) {
+	public void sendCountdown(ServerChannelPair pair) {
+		if (pair == null) {
 			return;
 		}
 
-		final var remainingDays = ChronoUnit.DAYS.between(LocalDate.now(), NW_RELEASE_DATE) - 1;
-		final var message = new StringBuilder("Good morning team!");
-
-		if (remainingDays == 0) {
-			message.append(" je popse!");
-			channel.createMessage(message.toString()).block();
-		} else {
-			message.append(" " + remainingDays + " je popse!");
-			channel.createMessage(message.toString()).block();
+		final var server = this.client.getGuildById(pair.getServerId()).block();
+		if (server == null) {
+			log.error("Could not get discord server with id {}", pair.getServerId());
+			return;
 		}
+
+		final var channel = server.getChannelById(pair.getChannelId()).block();
+		if (channel == null) {
+			log.error("Could not get discord channel with id {} (server {})", pair.getChannelId(), pair.getServerId());
+			return;
+		}
+
+		final var message = pair.getMessage();
+		channel.getRestChannel().createMessage(message).block();
 
 	}
 
--- src/main/java/com/stefanosdemetriou/discord/messaging/ServerChannelPair.java
+++ src/main/java/com/stefanosdemetriou/discord/messaging/ServerChannelPair.java
@@ -1,9 +1,16 @@
 package com.stefanosdemetriou.discord.messaging;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 import discord4j.common.util.Snowflake;
 import lombok.Getter;
 
 public class ServerChannelPair {
+
+	private static final LocalDate NW_RELEASE_DATE = LocalDate.parse("20210928",
+			DateTimeFormatter.ofPattern("yyyyMMdd"));
 
 	@Getter
 	private final Snowflake serverId;
@@ -11,8 +18,45 @@ public class ServerChannelPair {
 	@Getter
 	private final Snowflake channelId;
 
-	public ServerChannelPair(long serverId, long channelId) {
+	private final String lang;
+
+	public ServerChannelPair(long serverId, long channelId, String lang) {
 		this.serverId = Snowflake.of(serverId);
 		this.channelId = Snowflake.of(channelId);
+		this.lang = lang;
+	}
+
+	public String getMessage() {
+		final var remainingDays = ChronoUnit.DAYS.between(LocalDate.now(), NW_RELEASE_DATE) - 1;
+		final var message = new StringBuilder("Good morning team!");
+
+		switch (this.lang.toLowerCase()) {
+		case "el":
+			this.appendGreek(message, remainingDays);
+			break;
+		default:
+			this.appendEnglish(message, remainingDays);
+			break;
+		}
+
+		return message.toString();
+	}
+
+	private void appendGreek(StringBuilder message, long remainingDays) {
+		if (remainingDays == 0) {
+			message.append(" je popse!");
+		} else {
+			message.append(" " + remainingDays + " je popse!");
+		}
+	}
+
+	private void appendEnglish(StringBuilder message, long remainingDays) {
+		if (remainingDays == 0) {
+			message.append(" Last day! Get pumped!!");
+		} else if (remainingDays == 1) {
+			message.append(" " + remainingDays + " day and tonight left!");
+		} else {
+			message.append(" " + remainingDays + " day and tonight left!");
+		}
 	}
 }
